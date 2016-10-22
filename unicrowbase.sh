63c281e45dcfac9f50aa9fb99a972a9ed16edb8e
kenansubasi <kenansubasiceng@gmail.com>
2016-10-22 15:11:18 +0300

    Düzenleme

--- unicrowbase.sh
+++ unicrowbase.sh
@@ -1,6 +1,9 @@
 
 
-read -p """Django: 1
+read -p """
+Welcome Unicrowbase :)
+
+Django: 1
 Sass  : 2
 Exit  : 0
 
@@ -11,6 +14,8 @@ case $state in
     [0]* ) exit;;
     * ) exit;;
 esac
+
+base_dir=`pwd`
 
 if [ $state -eq 1 ]
 then
@@ -25,7 +30,7 @@ then
       then
         break;
       else
-        echo "Please enter a project name."
+        echo "Please enter a project name!"
       fi
     done
 
@@ -33,7 +38,6 @@ then
     while true; do
       read -p "Which version are you using python? ( [python3]/python2 ): " python_version
       case $python_version in
-          [python3]* ) python_version=${python_version:-python3}; break;;
           [python2]* ) python_version=${python_version:-python2}; break;;
           * ) python_version=${python_version:-python3}; break;;
       esac
@@ -68,34 +72,33 @@ Do yu confirm? ([Y]/N): """ confirm
   echo -e "\n---   Setup Begins   ---\n"
 
   # First Step #
-  base_dir=`pwd`
-  rm -rf settings
-  git clone https://gist.github.com/12427dc80be9cf1af2f773c50f5c9758.git settings
   echo -e "\n---  First Step   ---\n"
+  rm -rf unicrowbase_settings
+  git clone https://gist.github.com/12427dc80be9cf1af2f773c50f5c9758.git unicrowbase_settings
   ##############
 
   { # try
 
     # Create Virtualenv #
+    echo -e "\n---  Create Virtualenv   ---\n"
     mkdir $project_name; cd $project_name; mkdir backup log conf
     virtualenv -p $python_version web/env; cd web; source env/bin/activate
-    echo -e "\n---  Create Virtualenv   ---\n"
     #####################
 
 
     # Install Django #
+    echo -e "\n---  Install Django   ---\n"
     { # try
       pip install Django==$django_version
     } || { # catch
       pip install Django
     }
-    echo -e "\n---  Install Django   ---\n"
     ##################
 
 
     # Start Project #
-    django-admin startproject source
     echo -e "\n---  Start Project   ---\n"
+    django-admin startproject source
     ##################
 
 
@@ -103,30 +106,30 @@ Do yu confirm? ([Y]/N): """ confirm
     cd source/source; mkdir apps templates templatetags settings
 
     # Apps
-    cd apps; $base_dir/$1/web/source/manage.py startapp core; mv ../urls.py core/
+    cd apps; $base_dir/$project_name/web/source/manage.py startapp core; mv ../urls.py core/
 
     # Templatetags
     cd ..; touch templatetags/__init__.py
 
     # Settings
-    cp $base_dir/settings/settings_init.py settings/
+    echo -e "\n---  Configure Project   ---\n"
+    cp $base_dir/unicrowbase_settings/settings_init.py settings/
     mv settings/settings_init.py settings/__init__.py
-    mv settings.py settingsa/
+    mv settings.py settings/
     mv settings/settings.py settings/base.py
     sed -i -e 's/source.urls/source.apps.core.urls/g' settings/base.py
-    echo -e "\n---  Configure Project   ---\n"
     echo -e "\n---     End Setup    ---\n"
     #####################
 
   } || { # catch
     cd $base_dir
     rm -rf $project_name
-    echo -e "\nAn error occurred. Deleted Files!\n"
+    echo -e "\n------------------\n"; echo -e "An error occurred. Installation aborted!\n";
   }
 
   #####################
 
-  rm -rf $base_dir/settings
+  rm -rf $base_dir/unicrowbase_settings
   deactivate
 
   ##################
@@ -144,7 +147,7 @@ then
       then
         break;
       else
-        echo "Please enter a project name."
+        echo "Please enter a project name!"
       fi
     done
 
@@ -159,11 +162,11 @@ then
     done
 
     # Remote Address
-    read -p "Remote address?(Optional, example: https://github.com/unicrow/djangobase.git): " remote_address
+    read -p "Remote address?(Optional, example: https://github.com/unicrow/unicrowbase.git): " remote_address
 
     echo -e "\n------------------\n"
     read -p """Project Name = $project_name
-Python Version = $sassbase_version
+Sassbase Version = $sassbase_version
 Remote Adress = $remote_address
 Do yu confirm? ([Y]/N): """ confirm
     case $confirm in
@@ -185,35 +188,39 @@ Do yu confirm? ([Y]/N): """ confirm
 
   echo -e "\n---   Setup Begins   ---\n"
 
-  {
-    git clone --branch=$sassbase_version https://github.com/unicrow/sassbase.git $project_name
-  } || {
-    git clone https://github.com/unicrow/sassbase.git $sassbase_version
-    echo -e "\nVersion not found! Pull default version.\n"
+  { # try
+    echo -e "\n---  First Step   ---\n"
+    {
+      git clone --branch=$sassbase_version https://github.com/unicrow/sassbase.git $project_name
+    } || {
+      git clone https://github.com/unicrow/sassbase.git $sassbase_version
+      echo -e "\nVersion not found! Pull default version.\n"
+    }
+
+    cd $project_name; rm -rf .git/
+
+    if [ $remote_address ]
+    then
+      echo -e "\n---  Configure Remote Address   ---\n"
+      git init
+      git remote add origin $remote_address
+    fi
+
+    # Install Npm Packages #
+    echo -e "\n---  Install Npm Packages   ---\n"
+    npm install
+    ########################
+    echo -e "\n---     End Setup    ---\n"
+
+    echo -e "\n---     Start Grunt    ---\n"
+    # Start Grunt #
+    grunt
+    ###############
+  } || { # catch
+    cd $base_dir
+    rm -rf $project_name
+    echo -e "\n------------------\n"; echo -e "An error occurred. Installation aborted!\n";
   }
-
-  cd $project_name; rm -rf .git/
-  echo -e "\n---  First Step   ---\n"
-
-  if [ $remote_address ]
-  then
-    git init
-    git remote add origin $remote_address
-    echo -e "\n---  Configure Remote Address   ---\n"
-  fi
-
-  # Install Npm Packages #
-  npm install
-  ########################
-  echo -e "\n---  Install Npm Packages   ---\n"
-  echo -e "\n---     End Setup    ---\n"
-
-  echo -e "\n---     Start Grunt    ---\n"
-  # Start Grunt #
-  grunt
-  ###############
-
-  ##################
 
 else
   exit
