d4da42ca2e214ddb8e5e9fbdc465879113a965c1
kenansubasi <kenansubasiceng@gmail.com>
2016-10-26 15:17:47 +0300

    Sass için gerekli paketlerin kurulumu tamamlandı.

--- unicrowbase.sh
+++ unicrowbase.sh
@@ -11,13 +11,12 @@ echo -e -n """\033[0;96m
 read -p """
 Welcome Unicrowbase :)
 
-Start Django Project: 1
-Start Sass Project : 2
-Install Sass Requirements: 3
-Exit  : 0
+Start Django Project : 1
+Start Sass Project   : 2
+Exit                 : 0
 
-Which project are started? [Enter]: """ state
-case $state in
+Which project are started? [Enter]: """ start_state
+case $start_state in
     [1]* ) echo -e "\n---   Django Project  ---\n";;
     [2]* ) echo -e "\n---    Sass Project   ---\n";;
     [3]* ) echo -e "\n--- Sass Requirements ---\n";;
@@ -26,18 +25,21 @@ case $state in
 esac
 
 base_dir=`pwd`
+OS=`uname`
+echo -e "\033[0;32mOS: $OS\033[0;97m"
+echo -e "\n-------------------------\n"
 
-if [ $state -eq 1 ]
-then
+if [ $start_state -eq 1 ]; then
 
   ###   Django   ###
 
   while true; do
+    echo -e "\n---    First Step   ---\n"
+
     # Project Name
     while true; do
       read -p "What is the name of project? (project_name, home/user/project_name): " project_name
-      if [ $project_name ]
-      then
+      if [ $project_name ]; then
         break;
       else
         echo "Please enter a project name!"
@@ -46,11 +48,11 @@ then
 
     # Python Version
     while true; do
-      read -p "Which version are you using python? ( [python3]/python2 ): " python_version
-      case $python_version in
-          [python2]* ) python_version=${python_version:-python2}; break;;
-          * ) python_version=${python_version:-python3}; break;;
-      esac
+       read -p "Which version are you using python? ( [python3]/python2 ): " python_version
+       case $python_version in
+           [python2]* ) python_version=${python_version:-python2}; break;;
+           * ) python_version=${python_version:-python3}; break;;
+       esac
     done
 
     # Django Version
@@ -60,7 +62,7 @@ then
     # Remote Address
     read -p "Remote address? (Optional, example: https://github.com/unicrow/unicrowbase.git): " remote_address
 
-    echo -e "\n------------------\n"
+    echo -e "\n-----------------------\n"
     read -p """Project Name = $project_name
 Python Version = $python_version
 Django Version = $django_version
@@ -72,8 +74,7 @@ Do yu confirm? ([Y]/N): """ confirm
     esac
   done
 
-  if [ -d $project_name ]
-  then
+  if [ -d $project_name ]; then
     while true; do
       read -p "The project already exist! Did you overwrite? ( [N]/Y ): " remove_project
       case $remove_project in
@@ -86,7 +87,7 @@ Do yu confirm? ([Y]/N): """ confirm
   echo -e "\n---   Setup Begins   ---\n"
 
   # First Step #
-  echo -e "\n---  First Step   ---\n"
+  echo -e "\n---  Clone Code   ---\n"
   rm -rf unicrowbase_settings
   git clone https://gist.github.com/12427dc80be9cf1af2f773c50f5c9758.git unicrowbase_settings
   ##############
@@ -117,8 +118,7 @@ Do yu confirm? ([Y]/N): """ confirm
 
 
     # Configure Remote Address #
-    if [ $remote_address ]
-    then
+    if [ $remote_address ]; then
       echo -e "\n---  Configure Remote Address   ---\n"
       git init; git remote add origin $remote_address
     fi
@@ -157,17 +157,116 @@ Do yu confirm? ([Y]/N): """ confirm
 
   ##################
 
-elif [ $state -eq 2 ]
-then
+elif [ $start_state -eq 2 ]; then
 
   ###    Sass    ###
 
+  # Control Package #
+
+  echo -e "\n---   Control Packages   ---\n"
+  package=True
+  package_brew=True
+  package_npm=True
+  package_bower=True
+  package_grunt=True
+
+  {
+    brew --version >> unicrowbase.log
+    echo -en "\033[0;32mBrew \u2714 (`brew --version`)\033[0;97m\n"
+  } || {
+    package=False
+    package_brew=False
+    echo -en "\033[0;31mBrew \u2718 \033[0;97m\n"
+  }
+
+  {
+    npm --version >> unicrowbase.log
+    echo -e -n "\033[0;32mNpm \u2714 (`npm --version`)\033[0;97m\n"
+  } || {
+    package=False
+    package_npm=False
+    echo -e -n "\033[0;31mNpm \u2718 \033[0;97m\n"
+  }
+
+  {
+    bower --version >> unicrowbase.log
+    echo -e -n "\033[0;32mBower \u2714 (`bower --version`)\033[0;97m\n"
+  } || {
+    package=False
+    package_bower=False
+    echo -e -n "\033[0;31mBower \u2718 \033[0;97m\n"
+  }
+
+  {
+    grunt --version >> unicrowbase.log
+    echo -e -n "\033[0;32mGrunt \u2714 (`grunt --version`)\033[0;97m\n"
+  } || {
+    package=False
+    package_grunt=False
+    echo -e -n "\033[0;31mGrunt \u2718 \033[0;97m\n"
+
+    echo -e "\n---    Grunt     ---\n"
+    sudo npm install -g grunt-cli
+    echo -e "\n--------------------\n"
+  }
+
+  echo -e "\n----------------------------\n"
+
+  if [ $package == False ]; then
+
+    read -p "Is missing packages are installed?? ( [Y]/N ): " package_state
+    case $package_state in
+        [Nn]* ) echo -e "\nInstallation aborted!\n"; exit;;
+        * ) echo "";;
+    esac
+
+    # Brew #
+    if [ $package_brew == False ]; then
+      echo -e "\n---     Brew    ---\n"
+      if [ $OS == 'Linux' ]; then
+        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+      elif [ $OS == 'Darwin' ]; then
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+      fi
+      echo -e "\n--------------------\n"
+    fi
+    ########
+
+    # Npm #
+    if [ $package_npm == False ]; then
+      echo -e "\n---     Npm     ---\n"
+      brew install node
+      echo -e "\n--------------------\n"
+    fi
+    #######
+
+    # Bower #
+    if [ $package_bower == False ]; then
+      echo -e "\n---    Bower     ---\n"
+      sudo npm install -g bower
+      echo -e "\n--------------------\n"
+    fi
+    #######
+
+    # Grunt #
+    if [ $package_grunt == False ]; then
+      echo -e "\n---    Grunt     ---\n"
+      sudo npm install -g grunt-cli
+      echo -e "\n--------------------\n"
+    fi
+    #########
+
+  fi
+
+  rm -rf npm-debug.log
+  rm -rf unicrowbase.log
+
   while true; do
+    echo -e "\n---    First Step   ---\n"
     # Project Name
     while true; do
       read -p "What is the name of project? (project_name, home/user/project_name): " project_name
-      if [ $project_name ]
-      then
+      if [ $project_name ]; then
         break;
       else
         echo "Please enter a project name!"
@@ -187,7 +286,7 @@ then
     # Remote Address
     read -p "Remote address? (Optional, example: https://github.com/unicrow/unicrowbase.git): " remote_address
 
-    echo -e "\n------------------\n"
+    echo -e "\n-----------------------\n"
     read -p """Project Name = $project_name
 Sassbase Version = $sassbase_version
 Remote Adress = $remote_address
@@ -198,8 +297,7 @@ Do yu confirm? ([Y]/N): """ confirm
     esac
   done
 
-  if [ -d "$project_name" ]
-  then
+  if [ -d "$project_name" ]; then
     while true; do
       read -p "The project already exist! Did you overwrite? ( [N]/Y ): " remove_project
       case $remove_project in
@@ -209,12 +307,12 @@ Do yu confirm? ([Y]/N): """ confirm
     done
   fi
 
-  echo -e "\n---   Setup Begins   ---\n"
+  echo -e "\n\n---   Setup Begins   ---\n"
 
   { # try
 
     # First Step #
-    echo -e "\n---  First Step   ---\n"
+    echo -e "\n---  Clone Code   ---\n"
     {
       git clone --branch=$sassbase_version https://github.com/unicrow/sassbase.git $project_name
     } || {
@@ -235,16 +333,14 @@ Do yu confirm? ([Y]/N): """ confirm
 
 
     # Install Npm Packages #
-    if [ -f package.json ]
-    then
+    if [ -f package.json ]; then
       echo -e "\n---  Install Npm Packages   ---\n"
       npm install
     fi
     ########################
 
     # Bower Packages #
-    if [ -f bower.json ]
-    then
+    if [ -f bower.json ]; then
       echo -e "\n---  Install Bower Packages   ---\n"
       bower install
     fi
@@ -264,77 +360,5 @@ Do yu confirm? ([Y]/N): """ confirm
     rm -rf $project_name
     echo -e "\n------------------\n"; echo -e "An error occurred. Installation aborted!\n";
   }
-
-elif [ $state -eq 3 ]
-then
-  read -p """Packages to be installed:
-1) Brew (For install npm)
-2) Npm
-3) Grunt
-
-Continue? ( [Y]/N ): """ state
-  case $state in
-      [Nn]* ) echo -e "\n---   Installation aborted!   ---\n"; exit;;
-      * ) echo -e "\n---   Start Installation   ---\n";;
-  esac
-
-  OS=`uname`
-  echo $OS
-
-  if [ $OS == 'Linux' ]
-  then
-
-    echo -e "\n---     Brew    ---\n"
-    {
-      brew --version >> unicrowbase.log
-      echo "Brew already installed. (`brew --version`)"
-    } || {
-      ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
-    }
-    echo -e "\n--------------------\n"
-
-  elif [ $OS == 'Darwin' ]
-  then
-
-    echo -e "\n---     Brew    ---\n"
-    {
-      brew --version >> unicrowbase.log
-      echo "Brew already installed. (`brew --version`)"
-    } || {
-      /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    }
-    echo -e "\n--------------------\n"
-
-  fi
-
-  echo -e "\n---     Npm     ---\n"
-  {
-    npm --version >> unicrowbase.log
-    echo "Npm already installed. (`npm --version`)"
-  } || {
-    brew install node
-  }
-  echo -e "\n--------------------\n"
-
-  echo -e "\n---    Bower     ---\n"
-  {
-    bower --version >> unicrowbase.log
-    echo "Bower already installed. (`bower --version`)"
-  } || {
-    sudo npm install -g bower
-  }
-  echo -e "\n--------------------\n"
-
-  echo -e "\n---    Grunt     ---\n"
-  {
-    grunt --version >> unicrowbase.log
-    echo "Grunt already installed. (`grunt --version`)"
-  } || {
-    sudo npm install -g grunt-cli
-  }
-  echo -e "\n--------------------\n"
-
-  rm -rf npm-debug.log
-  rm -rf unicrowbase.log
 
 fi
